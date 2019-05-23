0c4c040b02db926530abd4761ba8470f83c61359
zubair-artani <zubairartani7@gmail.com>
2019-05-23 14:52:25 +0500

    Dashboard UI fixed

--- application/views/Dashboard/index.php
+++ application/views/Dashboard/index.php
@@ -20,61 +20,57 @@
       <div class="row">
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-aqua">
-            <div class="inner">
-              <h3>150</h3>
-
-              <p>New Orders</p>
+            <div class="bg-white" style="background: white;padding:20px;margin-bottom: 20px;" >
+            <h4 class="thumb-heading">CURRENT BALANCE</h4>
+            <div class="widget-thumb-wrap">
+            <i class="fa fa-lightbulb-o bg-green"></i>
+            <div class="widget-thumb-body">
+                <span class="widget-thumb-subtitle">USD</span>
+                <span class="widget-thumb-body-stat" data-counter="counterup" data-value="7,644">7,644</span>
             </div>
-            <div class="icon">
-              <i class="ion ion-bag"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-green">
-            <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-              <p>Bounce Rate</p>
+          <div class="bg-white" style="background: white;padding:20px;margin-bottom: 20px;">
+            <h4 class="thumb-heading">CURRENT BALANCE</h4>
+            <div class="widget-thumb-wrap">
+            <i class="fa fa-lightbulb-o bg-navy"></i>
+            <div class="widget-thumb-body">
+                <span class="widget-thumb-subtitle">USD</span>
+                <span class="widget-thumb-body-stat" data-counter="counterup" data-value="7,644">7,644</span>
             </div>
-            <div class="icon">
-              <i class="ion ion-stats-bars"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-yellow">
-            <div class="inner">
-              <h3>44</h3>
-
-              <p>User Registrations</p>
+          <div class="bg-white" style="background: white;padding:20px;margin-bottom: 20px;">
+            <h4 class="thumb-heading">CURRENT BALANCE</h4>
+            <div class="widget-thumb-wrap">
+            <i class="fa fa-lightbulb-o bg-red"></i>
+            <div class="widget-thumb-body">
+                <span class="widget-thumb-subtitle">USD</span>
+                <span class="widget-thumb-body-stat" data-counter="counterup" data-value="7,644">7,644</span>
             </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-red">
-            <div class="inner">
-              <h3>65</h3>
-
-              <p>Unique Visitors</p>
+          <div class="bg-white" style="background: white;padding:20px;margin-bottom: 20px;">
+            <h4 class="thumb-heading">CURRENT BALANCE</h4>
+            <div class="widget-thumb-wrap">
+            <i class="fa fa-lightbulb-o bg-purple"></i>
+            <div class="widget-thumb-body">
+                <span class="widget-thumb-subtitle">USD</span>
+                <span class="widget-thumb-body-stat" data-counter="counterup" data-value="7,644">7,644</span>
             </div>
-            <div class="icon">
-              <i class="ion ion-pie-graph"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
@@ -82,61 +78,57 @@
       <div class="row">
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-aqua">
-            <div class="inner">
-              <h3>150</h3>
-
-              <p>New Orders</p>
+          <div class="bg-white" style="background: white;padding:20px;margin-bottom: 20px;">
+            <h4 class="thumb-heading">CURRENT BALANCE</h4>
+            <div class="widget-thumb-wrap">
+            <i class="fa fa-lightbulb-o bg-blue"></i>
+            <div class="widget-thumb-body">
+                <span class="widget-thumb-subtitle">USD</span>
+                <span class="widget-thumb-body-stat" data-counter="counterup" data-value="7,644">7,644</span>
             </div>
-            <div class="icon">
-              <i class="ion ion-bag"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-green">
-            <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-              <p>Bounce Rate</p>
+          <div class="bg-white" style="background: white;padding:20px;margin-bottom: 20px;">
+            <h4 class="thumb-heading">CURRENT BALANCE</h4>
+            <div class="widget-thumb-wrap">
+            <i class="fa fa-lightbulb-o bg-red"></i>
+            <div class="widget-thumb-body">
+                <span class="widget-thumb-subtitle">USD</span>
+                <span class="widget-thumb-body-stat" data-counter="counterup" data-value="7,644">7,644</span>
             </div>
-            <div class="icon">
-              <i class="ion ion-stats-bars"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-yellow">
-            <div class="inner">
-              <h3>44</h3>
-
-              <p>User Registrations</p>
+          <div class="bg-white" style="background: white;padding:20px;margin-bottom: 20px;">
+            <h4 class="thumb-heading">CURRENT BALANCE</h4>
+            <div class="widget-thumb-wrap">
+            <i class="fa fa-lightbulb-o bg-purple"></i>
+            <div class="widget-thumb-body">
+                <span class="widget-thumb-subtitle">USD</span>
+                <span class="widget-thumb-body-stat" data-counter="counterup" data-value="7,644">7,644</span>
             </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-red">
-            <div class="inner">
-              <h3>65</h3>
-
-              <p>Unique Visitors</p>
+          <div class="bg-white" style="background: white;padding:20px;margin-bottom: 20px;">
+            <h4 class="thumb-heading">CURRENT BALANCE</h4>
+            <div class="widget-thumb-wrap">
+            <i class="fa fa-lightbulb-o bg-navy"></i>
+            <div class="widget-thumb-body">
+                <span class="widget-thumb-subtitle">USD</span>
+                <span class="widget-thumb-body-stat" data-counter="counterup" data-value="7,644">7,644</span>
             </div>
-            <div class="icon">
-              <i class="ion ion-pie-graph"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
